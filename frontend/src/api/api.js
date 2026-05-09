3f803da44491dccd730fe7c4350cb77fa4ee4d3c
malathi1945 <godiselamalathi@gmail.com>
2026-05-09 08:04:08 +0530

    Fixed API base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://task-management-backend-xh7y.onrender.com'
+  baseURL: 'https://task-management-backend-xh7y.onrender.com/api/v1'
 });
 
 API.interceptors.request.use((req) => {
