827d01f798da1853e5da59f5ccff0335c7f7df43
malathi1945 <godiselamalathi@gmail.com>
2026-05-09 07:45:47 +0530

    Updated frontend API URL for production

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api/v1'
+  baseURL: 'https://task-management-backend-xh7y.onrender.com'
 });
 
 API.interceptors.request.use((req) => {
